55fefd5c1bb7046599801fcb73dc2ab5a4375146
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-24 21:21:06 -0800

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int count=0;
+while(n!=0)
+{
+for(i=2;i<=n/2;i++)
+{
+count++
