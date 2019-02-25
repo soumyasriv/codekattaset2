f3bf330bb8ba3a3f17098e51752dd46e735547d1
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-25 05:36:47 -0800

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,11 +1,22 @@
 import java.util.Scanner;
+class Solution
+{
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
-int count=0;
-while(n!=0)
+int flag=0;
+for( int i=2;i<=n/2;i++)
 {
-for(i=2;i<=n/2;i++)
+if (n%i==0)
 {
-count++
+  System.out.println("no");
+  flag=1;
+  break;
+}
+}
+  if (flag==0)
+ 
+    System.out.println("yes");
+  
+}}
