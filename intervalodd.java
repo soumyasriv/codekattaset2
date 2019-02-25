13553e92c1feb33dabc4fd9081f94c62a08cf329
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-25 05:49:51 -0800

    Create intervalodd.java

---
+++ intervalodd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Solution
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int m=nextInt();
+int n=nextInt();
+for(int i=m+1;i<=n-1;i++);
+{
+if (i%2!=0)
+{
+System.out.println(i);
+}
+}
+}}
