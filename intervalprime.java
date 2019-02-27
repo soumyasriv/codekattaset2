1b194ceb701a32e1031017f578d56c6e6be3c3ce
soumyasriv <40382360+soumyasriv@users.noreply.github.com>
2019-02-26 19:52:09 -0800

    Create intervalprime.java

---
+++ intervalprime.java
@@ -0,0 +1,25 @@
+import java.util.Sacnner;
+class Solution
+{
+public static  void main (String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int m =sc.nextInt();
+while (low<high)
+{
+for (int i=2;i<=n/2;i++)
+{
+if (n%i==0)
+{
+flag=1;
+break;
+}
+if(!flag)
+{
+System.out.print(n+"")
+++n;
+}
+}}}}
+
+
